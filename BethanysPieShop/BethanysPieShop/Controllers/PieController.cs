a69a1c548449188a96a2d4252cd740f8cf813c9f
Joseph Abraham <josephseppu@outlook.com>
2018-12-01 13:10:53 +1100

    Added Pie Controller and View

---
+++ BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            //PiesListViewModel piesListViewModel = new PiesListViewModel();
+            //piesListViewModel.Pies = _pieRepository.Pies;
+
+            //piesListViewModel.CurrentCategory = "Cheese cakes";
+
+            //return View(piesListViewModel);
+
+            return View(_pieRepository.Pies);
+        }
+
+    }
+}
