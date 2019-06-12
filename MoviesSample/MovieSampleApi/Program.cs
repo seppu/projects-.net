93a9b13d0eb261af078f094ae6316714b0df2713
Joseph Abraham <josephseppu@gmail.com>
2019-06-12 14:57:44 +1000

    Added new WebApi Project and removed the mock project

--- MoviesSample/MovieSampleApi/Program.cs
+++ MoviesSample/MovieSampleApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace BethanysPieShop
+namespace MovieSampleApi
 {
     public class Program
     {
