443625703c95458d22400d33e9b993bfa2d4414b
Joseph Abraham <josephseppu@gmail.com>
2019-06-12 13:31:10 +1000

    Test Cases

--- unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -6,11 +6,11 @@ namespace Prime.Services
     {
         public bool IsPrime(int candidate)
         {
-            if (candidate == 1)
+            if (candidate < 2)
             {
                 return false;
             }
             throw new NotImplementedException("Please create a test first");
-        }
+        }        
     }
 }
