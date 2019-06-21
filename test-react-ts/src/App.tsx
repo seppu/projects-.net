803b071dbce7d0175fd1854951a802ccdf29d92c
Joseph Abraham <josephseppu@gmail.com>
2019-06-21 14:21:19 +1000

    Test

--- test-react-ts/src/App.tsx
+++ test-react-ts/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
+import StatefullHello from './components/StatefulHello';
 
 const App: React.FC = () => {
   return (
@@ -9,7 +11,10 @@ const App: React.FC = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          Test
         </p>
+        <Hello name="Joseph" enthusiasmLevel={10}></Hello>
+        <StatefullHello name="Joseph" enthusiasmLevel={20}></StatefullHello>
         <a
           className="App-link"
           href="https://reactjs.org"
