7e86c82378d8efc031eb9ce1d3fbce29b8f8c79a
Joseph Abraham <josephseppu@gmail.com>
2019-07-03 15:33:58 +1000

    Reducer test cases

--- test-react-ts/src/reducers/index.test.tsx
+++ test-react-ts/src/reducers/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { enthusiasm } from './index'
-import { INCREMENT_ENTHUSIASM } from '../constants/index';
+import { INCREMENT_ENTHUSIASM,DECREMENT_ENTHUSIASM } from '../constants/index';
 
 
 it('Reducer for Increment Enthusiasm', () => {
@@ -9,3 +9,8 @@ it('Reducer for Increment Enthusiasm', () => {
     expect(test1).toEqual({languageName: 'Test1', enthusiasmLevel: 21})
 });
 
+it('Reducer for Decrement Enthusiasm', () => {
+    let test1 = {languageName: 'Test1', enthusiasmLevel: 20}
+    test1 = enthusiasm(test1,{type: DECREMENT_ENTHUSIASM })
+    expect(test1).toEqual({languageName: 'Test1', enthusiasmLevel: 19})
+});
