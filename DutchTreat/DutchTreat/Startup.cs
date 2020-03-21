8931761bed482e2e7a60bb7e39e2b47fa33019fb
Joseph Abraham <josephseppu@gmail.com>
2020-03-21 12:37:27 +1100

    MVC changes

--- DutchTreat/DutchTreat/Startup.cs
+++ DutchTreat/DutchTreat/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +14,17 @@ namespace DutchTreat
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,9 +44,27 @@ namespace DutchTreat
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
-            app.UseDefaultFiles();
+            //app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseRouting();
+
             app.UseNodeModules();
+
+            
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Fallback",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", action = "Index" });
+            });
         }
     }
 }
--- DutchTreat/DutchTreat/wwwroot/js/index.js
+++ DutchTreat/DutchTreat/wwwroot/js/index.js
@@ -1,6 +1,44 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
 
-console.log("Hello Pluralsight");
+    console.log("Hello Pluralsight");
+
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("clicked one of the items: " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle(1000);
+    });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
