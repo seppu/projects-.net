b5e7a3a4c3e3969bc48e298291a17e24e9bdfb0d
Joseph Abraham <josephseppu@gmail.com>
2020-03-24 18:09:13 +1100

    Completed the MVC setup

--- DutchTreat/DutchTreat/Controllers/AppController.cs
+++ DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
+using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,14 +12,31 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService mailService;
+
+        public AppController(IMailService mailService)
+        {
+            this.mailService = mailService;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet("contact")]
         public IActionResult Contact()
+        {            
+            return View();
+        }
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
         {
-            ViewBag.Title = "Contact Us";
+            if (ModelState.IsValid)
+            {
+                mailService.SendMessage("josephseppu@gmail.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail send";
+                ModelState.Clear();
+            }
             return View();
         }
         public IActionResult About()
--- DutchTreat/DutchTreat/Startup.cs
+++ DutchTreat/DutchTreat/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,7 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, NullMailService>();
             services.AddControllersWithViews();
         }
 
@@ -48,6 +50,10 @@ namespace DutchTreat
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
             }
 
             app.UseHttpsRedirection();
