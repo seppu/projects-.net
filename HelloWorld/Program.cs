956835e2a95fd17fdad6131e8cb70ac3e86aaef8
joseph.abraham <joseph.abraham@dhhs.vic.gov.au>
2023-03-07 22:51:01 +1100

    Teaching ron git

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,6 +8,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Ron!");
             Console.WriteLine(JsonConvert.SerializeObject(args));
         }
     }
